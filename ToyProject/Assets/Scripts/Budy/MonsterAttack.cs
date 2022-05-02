6044b61a93c7f0e14685640059f757fc14eb6ceb
LJH <scoke0801@gmail.com>
2022-05-02 20:53:34 +0900

    몬스터가 시야범위 안에 들어오면 투사체 생성하도록

--- ToyProject/Assets/Scripts/Budy/MonsterAttack.cs
+++ ToyProject/Assets/Scripts/Budy/MonsterAttack.cs
@@ -11,10 +11,14 @@ public class MonsterAttack : MonoBehaviour
 {
     [SerializeField]
     private Status status;
+    private GameObject toCreatePrefab;
+
+    private List<GameObject> projectiles = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
+        toCreatePrefab = Resources.Load<GameObject>("Prefabs/TempProjectile");
         status = new Status();
     }
 
@@ -24,29 +28,40 @@ public class MonsterAttack : MonoBehaviour
         status.attackCoolTime = Mathf.Max(status.attackCoolTime - Time.deltaTime, 0.0f);
     }
 
-    void DoAttack()
+    void DoAttack(Collider target)
     {
         if ( status.attackCoolTime > 0.0f)
         {
             // 아직 쿨타임이 남아있는 경우
+            Debug.Log(" status.attackCoolTime > 0.0f ");
             return;
         }
 
         status.attackCoolTime += Constants.MONSTER_ATTACK_COOLTILE;
 
-        // To do
-        //
+        Vector3 targetPos = target.gameObject.transform.position;
+        Vector3 shootPos = transform.position;
+        shootPos.y += 1.5f;
+
+        GameObject newProjectile = Instantiate(toCreatePrefab, shootPos, Quaternion.Euler(0, 0, 0));
+        projectiles.Add(newProjectile);
+
+
+        Destroy(newProjectile, 5.0f);
+
+        Debug.Log("Created Projectile!!!");
+
         return;
     }
     private void OnTriggerEnter(Collider other)
     {  
         if ( other.gameObject.name == "Chicken(Clone)")
         {
-            DoAttack();
+            DoAttack(other);
         }
         else if (other.gameObject.name == "Conder(Clone)")
         {
-            DoAttack();
+            DoAttack(other);
         }
     }
      
