b9aa837313e2b61b0bbfc634de7365b606398935
LJH <scoke0801@gmail.com>
2022-07-08 23:37:47 +0900

    트리거 충돌 시 반응 추가 / 실제 처리 자체는 아직

---
+++ ToyProject/Assets/Scripts/LobbyEnterHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+enum SceneEnterTriggerType
+{
+    NONE = -1,
+    SHOP,
+    GAME,
+}
+
+public class LobbyEnterHelper : MonoBehaviour
+{
+    [SerializeField] SceneEnterTriggerType sceneEnterTrigerType;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (sceneEnterTrigerType)
+        {
+            case SceneEnterTriggerType.SHOP:
+                {
+                    Debug.Log("OnTriggerEnter < case SceneEnterTriggerType.SHOP");
+                }break;
+            case SceneEnterTriggerType.GAME:
+                { 
+                }break;
+            default:
+                { 
+                }break;
+        }
+    }
+}
+ 
