518f4a65d64a55f99324b0faf45f4438e40bb424
LJH <scoke0801@gmail.com>
2022-08-09 21:10:41 +0900

    임시 주석 처리

--- ToyProject/Assets/Scripts/SceneChanger.cs
+++ ToyProject/Assets/Scripts/SceneChanger.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using GooglePlayGames;
-using GooglePlayGames.BasicApi;
+using GooglePlayGames.BasicApi; 
 
 public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         //구성 및 초기화
         //PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder().Build());
         //디버깅에 권장됨
-        PlayGamesPlatform.DebugLogEnabled = true;
-        //PlayGamesPlatform 활성화
-        PlayGamesPlatform.Activate();
+        //PlayGamesPlatform.DebugLogEnabled = true;
+        ////PlayGamesPlatform 활성화
+        //PlayGamesPlatform.Activate();
     }
 
     // Update is called once per frame
@@ -26,15 +26,15 @@ public class SceneChanger : MonoBehaviour
 
     public void OnClickLogin()
     {
-        if (PlayGamesPlatform.Instance.IsAuthenticated() == false)
-        { 
-            Social.localUser.Authenticate((bool isSuccess, string errorMsz) =>
-           {
-               Debug.Log("login attemp" + isSuccess + " " + errorMsz);
-               //to do
-               SceneManager.LoadScene("Scenes/Lobby"); 
-           });
-        } 
+        //if (PlayGamesPlatform.Instance.IsAuthenticated() == false)
+        //{ 
+        //    Social.localUser.Authenticate((bool isSuccess, string errorMsz) =>
+        //   {
+        //       Debug.Log("login attemp" + isSuccess + " " + errorMsz);
+        //       //to do
+        //       SceneManager.LoadScene("Scenes/Lobby"); 
+        //   });
+        //} 
     }
 
     public void OnClickLogout()
